8daeb84eb1d118b8c161b50e591e26fd2303e224
lilmayu <uwulilmayu@gmail.com>
2021-06-27 10:30:46 +0200

    Bukkit coloring

---
+++ src/main/java/lilmayu/logging/coloring/BukkitColoring.java
@@ -0,0 +1,14 @@
+package lilmayu.logging.coloring;
+
+import lilmayu.logging.types.*;
+
+public class BukkitColoring extends BaseColoring {
+
+    public BukkitColoring() {
+        addColoring(new ColoringString(new InfoLogType(), "§7"));
+        addColoring(new ColoringString(new WarningLogType(), "§e"));
+        addColoring(new ColoringString(new ErrorLogType(), "§c"));
+        addColoring(new ColoringString(new DebugLogType(), "§8"));
+        addColoring(new ColoringString(new TraceLogType(), "§5"));
+    }
+}
